fa1fb2917dc4772d7c43cfab8e7eef1afd029f33
Mizanur Remon <mr248903@gmail.com>
2020-10-25 01:57:26 +0600

    25 Oct 2020 update

--- app/src/main/java/com/example/chitchat/resetpassword1.java
+++ app/src/main/java/com/example/chitchat/resetpassword1.java
@@ -11,16 +11,18 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class resetpassword1 extends AppCompatActivity implements View.OnClickListener{
+public class resetpassword1 extends AppCompatActivity implements View.OnClickListener {
 
     EditText getEmail;
     Button submitButton;
+    ImageButton backButton;
 
     FirebaseAuth mAuth;
 
@@ -31,29 +33,42 @@ public class resetpassword1 extends AppCompatActivity implements View.OnClickLis
 
         try {
             this.getSupportActionBar().hide();
-        }catch (NullPointerException e){}
+        } catch (NullPointerException e) {
+        }
 
         getEmail = (EditText) findViewById(R.id.resetemail);
         submitButton = (Button) findViewById(R.id.submitresetID);
+        backButton = (ImageButton) findViewById(R.id.backID);
 
         mAuth = FirebaseAuth.getInstance();
 
         submitButton.setOnClickListener(this);
+        backButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        startActivity(new Intent(resetpassword1.this, Login.class));
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId()==R.id.submitresetID){
+        if (v.getId() == R.id.submitresetID) {
             String email = getEmail.getText().toString().trim();
 
-            if(!TextUtils.isEmpty(email)){
+            if (!TextUtils.isEmpty(email)) {
                 reset_link_sent(email);
-            }else {
+            } else {
                 Toast toast = Toast.makeText(resetpassword1.this, "Enter Your mail", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0 ,0);
+                toast.setGravity(Gravity.CENTER, 0, 0);
                 toast.show();
             }
 
+        }
+        else if(v.getId()==R.id.backID){
+            finish();
+            startActivity(new Intent(resetpassword1.this, Login.class));
         }
     }
 
@@ -61,18 +76,20 @@ public class resetpassword1 extends AppCompatActivity implements View.OnClickLis
         mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Toast toast = Toast.makeText(resetpassword1.this, "A link has been sent to your mail to reset your password",Toast.LENGTH_SHORT);
+                if (task.isSuccessful()) {
+                    Toast toast = Toast.makeText(resetpassword1.this, "A link has been sent to your mail to reset your password", Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER, 0, 0);
                     toast.show();
 
                     startActivity(new Intent(resetpassword1.this, resetpassword2.class));
-                }else {
-                    Toast toast = Toast.makeText(resetpassword1.this, "Error occured",Toast.LENGTH_SHORT);
+                } else {
+                    Toast toast = Toast.makeText(resetpassword1.this, "Error occured", Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER, 0, 0);
                     toast.show();
                 }
             }
         });
     }
+
 }
+
--- app/src/main/java/com/example/chitchat/splashscreen.java
+++ app/src/main/java/com/example/chitchat/splashscreen.java
@@ -2,14 +2,16 @@ package com.example.chitchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 import android.widget.VideoView;
 
 public class splashscreen extends AppCompatActivity {
-
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,15 @@ public class splashscreen extends AppCompatActivity {
             this.getSupportActionBar().hide();
         }catch (NullPointerException e){}
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading");
+        progressDialog.show();
+
         long SPLASH_TIME_OUT = 2500;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                progressDialog.dismiss();
                 startActivity(new Intent(splashscreen.this, MainActivity.class));
                 finish();
             }
