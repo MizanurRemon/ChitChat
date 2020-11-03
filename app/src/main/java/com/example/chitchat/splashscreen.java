06ec90f64425613b15a5656e28c1352ab2cde627
Mizanur Remon <mr248903@gmail.com>
2020-11-03 14:40:04 +0600

    03 Nov 2020 update

--- app/src/main/java/com/example/chitchat/splashscreen.java
+++ app/src/main/java/com/example/chitchat/splashscreen.java
@@ -1,40 +1,46 @@
 package com.example.chitchat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.ProgressBar;
-import android.widget.VideoView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class splashscreen extends AppCompatActivity {
-    private ProgressDialog progressDialog;
+
+    TextView textView;
+    ImageView imageView;
+    Animation fading_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        textView = (TextView) findViewById(R.id.textview);
+        imageView = (ImageView) findViewById(R.id.imageview);
 
         try {
             this.getSupportActionBar().hide();
-        }catch (NullPointerException e){}
+        } catch (NullPointerException e) {
+        }
 
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading");
-        progressDialog.show();
+        fading_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        imageView.startAnimation(fading_in);
+        textView.startAnimation(fading_in);
 
         long SPLASH_TIME_OUT = 2500;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                progressDialog.dismiss();
+
                 startActivity(new Intent(splashscreen.this, MainActivity.class));
                 finish();
             }
-        },SPLASH_TIME_OUT);
+        }, SPLASH_TIME_OUT);
 
 
     }
